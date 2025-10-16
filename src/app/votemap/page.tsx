a5985ebcdb6c1694302f96396c80e6fa7c6d9563
KaiWelu <kaiweluda@gmail.com>
2025-10-16 16:26:55 +0200

    adds working csv parsing

--- src/app/votemap/page.tsx
+++ src/app/votemap/page.tsx
@@ -2,22 +2,64 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Map, Source, Layer, MapRef } from "@vis.gl/react-maplibre";
 import type {
+  ExpressionSpecification,
   FillLayerSpecification,
   LineLayerSpecification,
 } from "maplibre-gl";
 import type { FeatureCollection, Feature } from "geojson";
+import Papa from "papaparse";
+
+interface CsvRow {
+  Wahlbezirk: string;
+  Wahlbezirksart: string;
+  Abgeordnetenhauswahlkreis: string;
+  "Die Linke": string;
+}
 
 const VoteMap = () => {
   const [geoData, setGeoData] = useState<FeatureCollection | null>(null);
+  const [csvData, setCsvData] = useState<Record<string, number>>({});
 
   const mapRef = useRef<MapRef>(null);
 
-  useEffect(() => {
+  const csvText = useEffect(() => {
     fetch("/data/Stimmbezirke-AGH21.geojson")
       .then((res) => res.json())
       .then((data) => setGeoData(data))
       .catch((err) => console.error("Error loading GeoJSON:", err));
+
+    // this will create a record with voting data for die Linke
+    // it uses "Erststimme"
+    fetch("/data/Berlin_BT25_W1.csv")
+      .then((res) => res.text())
+      .then((text) => {
+        Papa.parse<CsvRow>(text, {
+          header: true,
+          skipEmptyLines: true,
+          complete: (results) => {
+            const lookup: Record<string, number> = {};
+            results.data.forEach((row) => {
+              const combinedId = `0${row.Abgeordnetenhauswahlkreis}${row.Wahlbezirk}`;
+              if (!lookup[combinedId]) {
+                lookup[combinedId] = Number(row["Die Linke"]); // use any column you want
+              }
+            });
+            setCsvData(lookup);
+          },
+        });
+      })
+      .catch(console.error);
+
+    console.log(csvData);
   }, []);
+
+  const getColor = (votes: number) => {
+    if (votes > 100) {
+      return "#BF092F";
+    } else {
+      return "#3B9797";
+    }
+  };
 
   const handleClick = (e: any) => {
     if (!mapRef.current || !geoData) return;
@@ -27,6 +69,7 @@ const VoteMap = () => {
 
     if (!features.length) return;
     console.log("Clicked UWB:", features[0].properties?.UWB);
+    console.log(csvData);
   };
 
   const districtsFill = {
@@ -34,7 +77,15 @@ const VoteMap = () => {
     type: "fill",
     source: "voting-disctricts",
     paint: {
-      "fill-color": "#4C763B",
+      "fill-color": [
+        "match",
+        ["get", "combinedId"],
+        ...Object.entries(csvData).flatMap(([id, votes]) => [
+          id,
+          getColor(votes),
+        ]),
+        "#cccccc", // fallback color
+      ] as unknown as ExpressionSpecification,
       "fill-opacity": 0.2,
     },
     //need to check how the filter works
