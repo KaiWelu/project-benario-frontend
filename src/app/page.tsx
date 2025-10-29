fa7429a46d4ca60fa5917975aa29f669286fd33f
KaiWelu <kaiweluda@gmail.com>
2025-10-29 11:57:44 +0100

    adds fonts to globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center-safe text-red-600 p-60">
-      <h1 className="font-heading text-9xl font-bold ">
-        Welcome to the Jungle!
-      </h1>
-      <p className="font-sans text-8xl font-extralight">
+      <h1 className="font-heading text-9xl">Welcome to the Jungle!</h1>
+      <p className="font-sans text-8xl">
         We got fun and games, we got everything you want!
       </p>
     </div>
